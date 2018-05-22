6fa35af3e47ca44029d22951787a8b9354314a69
Papabyte <bencreze@yahoo.fr>
2018-05-22 12:18:56 +0200

    add conf.js

---
+++ conf.js
@@ -0,0 +1,16 @@
+
+exports.bServeAsHub = false;
+exports.bLight = true;
+exports.bSingleAddress = true;
+
+// Database
+exports.storage = 'sqlite';
+
+exports.bIgnoreUnpairRequests = true;
+exports.hub = 'byteball.org/bb';
+exports.deviceName = 'Private chat room';
+exports.permanent_pairing_secret = '0000';
+exports.control_addresses = [''];
+
+
+console.log('finished Private chat room configuration');
